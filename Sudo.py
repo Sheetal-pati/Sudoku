17106634edf0a3ff3ada3128b0acda3fc30c9719
SHEETAL PATI <36594056+Sheetal-pati@users.noreply.github.com>
2018-09-29 05:42:30 -0700

    Update Sudo.py

--- Sudo.py
+++ Sudo.py
@@ -6,41 +6,162 @@ def Check_input():
 	sub_y=0
 	while (i<9):
 		if(Sudo[i][y]==value):
-		flag1=1
-		break
+		   flag1=1
+		   break
+		else:
+		   i+=1
 	if(flag1==0):
 		while (j<9):
 			if(Sudo[x][j]==value):
 				flag1=1
 				break
+			else:
+		        j+=1
 	if (flag1==0):
 		if(x==0||x==1||x==2):
 			if(y==0||y==1||y==2):
 				print("case 1")
+				while ((sub_x<3) && (flag1==0)):
+					while (sub_y<3):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
+					
 
             if(y==3||y==4||y==5):
             	print("case 2")
+            	while ((sub_x<3) && (flag1==0)):
+					while ((sub_y<6)&&(sub_y>2)):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
             if(y==6||y==7||y==8):
                 print("case 3")	
+                while ((sub_x<3) && (flag1==0)):
+					while ((sub_y<9)&&(sub_y>5)):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
 
 		if(x==3||x==4||x==5):
 			if(y==0||y==1||y==2):
 				print("case 4")
+				while ((sub_x>2) && (sub_x<6)&&(flag1==0)):
+					while (sub_y<3):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
             if(y==3||y==4||y==5):
             	print("case 5")
+            	while ((sub_x>2) && (sub_x<6)&& (flag1==0)):
+					while ((sub_y<6)&&(sub_y>2)):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
             if(y==6||y==7||y==8):
             	print("case 6")
-
+                while ((sub_x>2) && (sub_x<6)&& (flag1==0)):
+					while ((sub_y<9)&&(sub_y>5)):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
 
 		if(x==6||x==7||x==8):
 			if(y==0||y==1||y==2):
 				print("case 7")
+				while ((sub_x<9) && (sub_x>5)&& (flag1==0)):
+					while (sub_y<3):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
             if(y==3||y==4||y==5):
             	print("case 8")
+            	while ((sub_x<9) && (sub_x>5)&& (flag1==0)):
+					while ((sub_y<6)&&(sub_y>2)):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
             if(y==6||y==7||y==8):
             	print("case 9")
+            	while ((sub_x<9) && (sub_x>5)&& (flag1==0)):
+					while ((sub_y<9)&&(sub_y>5)):
+						if(Sudo[sub_x][sub_y]==value):
+						   flag1=1;
+						   break
+						else:
+							sub_y+=1
+                    sub_x+=1
+                if(flag1==0):
+                	Sudo[x][y]=value
+                	return 1
+                else:
+                    return 0	
 
-def Ease():
+def Easy():
 	num=random.randint(30,35)
 	i=0
 	j=0
@@ -52,6 +173,46 @@ def Ease():
 			x=random.randint(-1,9)
 		    y=random.randint(-1,9)
             flag=Check_input()
+def Medium():
+	num=random.randint(25,30)
+	i=0
+	j=0
+	count=0
+	while(count<num):
+		value=random.randint(0,10)
+		flag=0
+		while(flag==0):
+			x=random.randint(-1,9)
+		    y=random.randint(-1,9)
+            flag=Check_input()
+
+def Hard():
+	num=random.randint(20,25)
+	i=0
+	j=0
+	count=0
+	while(count<num):
+		value=random.randint(0,10)
+		flag=0
+		while(flag==0):
+			x=random.randint(-1,9)
+		    y=random.randint(-1,9)
+            flag=Check_input()
+
+def Expert():
+	num=random.randint(15,20)
+	i=0
+	j=0
+	count=0
+	while(count<num):
+		value=random.randint(0,10)
+		flag=0
+		while(flag==0):
+			x=random.randint(-1,9)
+		    y=random.randint(-1,9)
+            flag=Check_input()
+	
+
 
 
 	
